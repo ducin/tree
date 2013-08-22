d9be23d020962aa06b62a89e5335d5e93cd330e2
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-22 23:26:34 +0200

    added node.js support

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+0.1.1
+-----
+
+Added node.js support
+
+0.1.0
+-----
+
+Initial release
--- src/tree.js
+++ src/tree.js
@@ -1,111 +1,132 @@
-/**
- * This list represents single level list of elements.
- * Each of elements has a children list (which is one level below).
- */
-function Tree() {
-	this.children = {};
-}
+(function(){
 
-Tree.prototype = [];
-Tree.prototype.constructor = Tree;
+	// establish the root object
+	var root = this;
 
-Tree.prototype.push = function() {
-	Array.prototype.push.apply(this, arguments);
-	var args = Array.prototype.slice.call(arguments);
-	for (var i = 0; i < args.length; i++) {
-		this.children[args[i]] = new Tree();
+	// temporary variables to speed up loading the library
+	var ArrayProto = Array.prototype
+	var push = ArrayProto.push;
+	var slice = ArrayProto.slice;
+
+	/**
+	 * This list represents single level list of elements.
+	 * Each of elements has a children list (which is one level below).
+	 */
+	var Tree = function() {
+		this.children = {};
 	}
-	return this.length;
-};
 
-Tree.prototype.pop = function() {
-	var element = Array.prototype.pop.call(this),
-		children = this.children[element];
-	delete this.children[element];
-	return {
-		element: element,
-		children: children
+	Tree.prototype = [];
+
+	Tree.prototype.constructor = Tree;
+
+	Tree.prototype.push = function() {
+		push.apply(this, arguments);
+		var args = slice.call(arguments);
+		for (var i = 0; i < args.length; i++) {
+			this.children[args[i]] = new Tree();
+		}
+		return this.length;
 	};
-};
 
-Tree.prototype.shift = function() {
-	var element = Array.prototype.shift.call(this),
-		children = this.children[element];
-	delete this.children[element];
-	return {
-		element: element,
-		children: children
+	Tree.prototype.pop = function() {
+		var element = ArrayProto.pop.call(this),
+			children = this.children[element];
+		delete this.children[element];
+		return {
+			element: element,
+			children: children
+		};
 	};
-};
 
-Tree.prototype.unshift = function() {
-	Array.prototype.unshift.apply(this, arguments);
-	var args = Array.prototype.slice.call(arguments);
-	for (var i = 0; i < args.length; i++) {
-		this.children[args[i]] = new Tree();
+	Tree.prototype.shift = function() {
+		var element = ArrayProto.shift.call(this),
+			children = this.children[element];
+		delete this.children[element];
+		return {
+			element: element,
+			children: children
+		};
+	};
+
+	Tree.prototype.unshift = function() {
+		ArrayProto.unshift.apply(this, arguments);
+		var args = slice.call(arguments);
+		for (var i = 0; i < args.length; i++) {
+			this.children[args[i]] = new Tree();
+		}
+		return this.length;
+	};
+
+	/**
+	 * This method can be chained, e.g.
+	 * tree.childrenOf(el1).childrenOf(el2) etc.
+	 */
+	Tree.prototype.childrenOf = function(element) {
+		return this.children[element];
+	};
+
+	Tree.prototype.nodes = function() {
+		return slice.call(this);
+	};
+
+	Tree.prototype.sort = function(compareFunction) {
+		// first, sort myself
+		if (this.length > 1) {
+			ArrayProto.sort.call(this, compareFunction);
+		}
+		// then, recursively sort all children subtrees by accessing
+		// this.children map where the key is the index of element in this array
+		for (var i = 0; i < this.length; i++) {
+			this.children[this[i]].sort(compareFunction);
+		}
+	};
+
+	Tree.prototype.reverse = function() {
+		// first, reverse myself
+		if (this.length > 1) {
+			ArrayProto.reverse.call(this);
+		}
+		// then, recursively reverse all children subtrees by accessing
+		// this.children map where the key is the index of element in this array
+		for (var i = 0; i < this.length; i++) {
+			this.children[this[i]].reverse();
+		}
+	};
+
+	/**
+	 * Returns flattened list join.
+	 * @param separator String
+	 * @return String
+	 */
+	/*
+	Tree.prototype.join = function(separator) {
+		return ArrayProto.join.call(Tree.prototype.flatten.call(this), separator);
+	};
+	*/
+
+	/**
+	 * Dumps tree into a flat list, preserving all levels ordering.
+	 * This function is recursive and takes one argument. From the outside it
+	 * should be called without any parameters
+	 * @return Array
+	 */
+	Tree.prototype.flatten = function() {
+		var nodeIds = [], nodeId;
+		for (var i = 0; i < this.length; i++) {
+			nodeId = this[i];
+			nodeIds.push(nodeId);
+			push.apply(nodeIds, this.childrenOf(nodeId).flatten());
+		}
+		return nodeIds;
+	};
+
+	if (typeof exports !== 'undefined') {
+		if (typeof module !== 'undefined' && module.exports) {
+			exports = module.exports = Tree;
+		}
+		exports.Tree = Tree;
+	} else {
+		root.Tree = Tree;
 	}
-	return this.length;
-};
-
-/**
- * This method can be chained, e.g.
- * tree.childrenOf(el1).childrenOf(el2) etc.
- */
-Tree.prototype.childrenOf = function(element) {
-	return this.children[element];
-};
-
-Tree.prototype.nodes = function() {
-	return Array.prototype.slice.call(this);
-};
-
-Tree.prototype.sort = function(compareFunction) {
-	// first, sort myself
-	if (this.length > 1) {
-		Array.prototype.sort.call(this, compareFunction);
-	}
-	// then, recursively sort all children subtrees by accessing
-	// this.children map where the key is the index of element in this array
-	for (var i = 0; i < this.length; i++) {
-		this.children[this[i]].sort(compareFunction);
-	}
-};
-
-Tree.prototype.reverse = function() {
-	// first, reverse myself
-	if (this.length > 1) {
-		Array.prototype.reverse.call(this);
-	}
-	// then, recursively reverse all children subtrees by accessing
-	// this.children map where the key is the index of element in this array
-	for (var i = 0; i < this.length; i++) {
-		this.children[this[i]].reverse();
-	}
-};
-
-/**
- * Returns flattened list join.
- * @param separator String
- * @return String
- */
-/*
-Tree.prototype.join = function(separator) {
-	return Array.prototype.join.call(Tree.prototype.flatten.call(this), separator);
-};
-*/
-
-/**
- * Dumps tree into a flat list, preserving all levels ordering.
- * This function is recursive and takes one argument. From the outside it
- * should be called without any parameters
- * @return Array
- */
-Tree.prototype.flatten = function() {
-	var nodeIds = [], nodeId;
-	for (var i = 0; i < this.length; i++) {
-		nodeId = this[i];
-		nodeIds.push(nodeId);
-		Array.prototype.push.apply(nodeIds, this.childrenOf(nodeId).flatten());
-	}
-	return nodeIds;
-};
+}());
